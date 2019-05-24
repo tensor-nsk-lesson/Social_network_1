10a1b576f9349f18801069f0056b61674e16ec3b
Lioran <wersil@mail.ru>
2019-05-25 02:48:05 +0700

    Пересмотрена концепция таблиц, переписаны скрипты

---
+++ sql/create.sql
@@ -0,0 +1,127 @@
+drop table "GlobalContent";
+drop table "LocalContent";
+drop table "Walls";
+drop table "UsersGroup";
+drop table "Group";
+drop table "Friends";
+drop table "Message" CASCADE;
+drop table "Dialogs";
+drop table "SeeingFriends";
+drop table "SeeingGroup";
+drop table "Token" CASCADE;
+drop table "FakeProfile" CASCADE;
+drop table "Privacy" CASCADE;
+drop table "Profile" CASCADE;
+drop table "Authorization" CASCADE;
+
+create table if not exists "Authorization"(
+	"Login" VARCHAR(20),
+	"Password" INTEGER NOT NULL,
+	"Id" INTEGER NOT NULL UNIQUE,
+	CONSTRAINT "pk_login" PRIMARY KEY ("Login")
+);
+create table if not exists "Profile"(
+	"Id" INTEGER,
+	"Photo" Text,
+	"SecondName" Text,
+	"FirstName" Text NOT NULL,
+	"FatherName" Text,
+	"Age" INTEGER,
+	"AboutMe" Text,
+	"Status" text,
+	"Date" timestamp without time zone,
+	"LastActivity" timestamp without time zone,
+	"StatusProfile" Text,
+	"Gender" boolean,
+	"Wall" INTEGER NOT NULL,
+	"IdContent" INTEGER NOT NULL,
+	CONSTRAINT "pk_id" PRIMARY KEY ("Id"),
+	FOREIGN KEY ("Id") REFERENCES "Authorization"("Id") ON DELETE CASCADE
+);
+create table if not exists "Privacy"(
+	"Id" INTEGER,
+	"Invisibility" boolean,
+	"FakeId" INTEGER UNIQUE,
+	"ViewFriends" INTEGER,
+	"ViewGroups" INTEGER,
+	CONSTRAINT "pk_id_private" PRIMARY KEY ("Id"),
+	FOREIGN KEY ("Id") REFERENCES "Profile"("Id") ON DELETE CASCADE
+);
+create table if not exists"FakeProfile"(
+	"Id" INTEGER,
+	"Photo" Text,
+	"SecondName" Text,
+	"FirstName" Text,
+	"FatherName" Text,
+	"Age" INTEGER,
+	"AboutMe" Text,
+	"Status" text,
+	"Date" timestamp without time zone,
+	"LastActivity" timestamp without time zone,
+	"StatusProfile" Text,
+	"Gender" boolean,
+	"Wall" INTEGER NOT NULL,
+	"IdContent" INTEGER NOT NULL,
+	CONSTRAINT "pk_id_faik" PRIMARY KEY ("Id"),
+	FOREIGN KEY ("Id") REFERENCES "Privacy"("FakeId") ON DELETE CASCADE
+);
+create table if not exists "Token"(
+	"IdUser" INTEGER NOT NULL,
+	"Password" INTEGER NOT NULL,
+	"Time" timestamp without time zone,
+	CONSTRAINT "pk_id_user" PRIMARY KEY ("IdUser"),
+	FOREIGN KEY ("IdUser") REFERENCES "Authorization"("Id") ON DELETE CASCADE
+);
+create table if not exists "SeeingGroup"(
+	"IdStatus" INTEGER NOT NULL,
+	"IdUser" INTEGER NOT NULL
+);
+create table if not exists "SeeingFriends"(
+	"IdStatus" INTEGER NOT NULL,
+	"IdUser" INTEGER NOT NULL
+);
+create table if not exists "Dialogs"(
+	"IdDialog" INTEGER NOT NULL,
+	"IdUser" INTEGER NOT NULL,
+	"Status" INTEGER NOT NULL,
+	"Name" Text
+);
+create table if not exists "Message"(
+	"IdDialog" INTEGER NOT NULL,
+	"Message" Text,
+	"Time" timestamp without time zone,
+	"Status" INTEGER NOT NULL
+);
+create table if not exists "Friends"(
+	"IdUser" INTEGER NOT NULL,
+	"IdFriend" INTEGER NOT NULL,
+	"Status" INTEGER NOT NULL,
+	"WideStatus" Text
+);
+create table if not exists "Group"(
+	"IdGroup" INTEGER,
+	"Name" Text,
+	"Description" Text,
+	CONSTRAINT "pk_id_group" PRIMARY KEY ("IdGroup")
+);
+create table if not exists "UsersGroup"(
+	"IdGroup" INTEGER NOT NULL,
+	"IdUser" INTEGER NOT NULL,
+	FOREIGN KEY ("IdGroup") REFERENCES "Group"("IdGroup") ON DELETE CASCADE
+);
+create table if not exists "Walls"(
+	"IdWall" INTEGER NOT NULL,
+	"Content" Text,
+	"Like" INTEGER NOT NULL,
+	"Dislike" INTEGER NOT NULL,
+	"Time" timestamp without time zone
+);
+create table if not exists "LocalContent"(
+	"IdContent" INTEGER NOT NULL,
+	"IdFile" INTEGER NOT NULL,
+	"File" Text
+);
+create table if not exists "GlobalContent"(
+	"IdFile" INTEGER NOT NULL,
+	"File" Text
+);
---
+++ sql/insert.sql
@@ -0,0 +1,45 @@
+insert into "Authorization" ("Login", "Password", "Id") VALUES ('Person1', 1, 101),
+							      ('Person2', 2, 102),
+							      ('Person3', 3, 103),
+							      ('Person4', 4, 104);
+
+insert into "Profile" ("Id", "SecondName", "FirstName", "FatherName", "Age", "AboutMe", "Status", "Date", "LastActivity","StatusProfile", "Gender", "Wall")
+VALUES (101, 'Воландеморт', 'Олег', 'Витальевич', 18, 'Князь темного угла', 'в активном поиске', to_timestamp('19.05.97', 'dd-mm-yy'), to_timestamp('20:21', 'hh24:mi'), 'онлайн', false, 901),
+(102, null, 'Лакистрайк', 'Анатольевич', 118, 'Холоп светлой стены','в активном поиске', to_timestamp('17.05.97', 'dd-mm-yy'), to_timestamp('20:31', 'hh24:mi'), 'онлайн', true, 902),
+(103, null, 'Каблук', null, 50, 'Мне жена не разрешила писать здесь что-то', 'в активном поиске', to_timestamp('12.05.97', 'dd-mm-yy'), to_timestamp('21:21', 'hh24:mi'), 'онлайн', false, 903),
+(104, null, 'Король', 'Треф', 88, null, 'в активном поиске', to_timestamp('11.05.97', 'dd-mm-yy'), to_timestamp('22:21', 'hh24:mi'), 'онлайн', true, 904);
+
+insert into "Privacy" ("Id", "Invisibility", "FakeId", "ViewFriends", "ViewGroups") VALUES (101, false, null, 0, 0),
+											 (102, true, null, 1, 1),
+											 (103, false, 201, 2, 1),
+											 (104, false, 202, 1, 0);
+
+insert into "FakeProfile" ("Id", "SecondName", "FirstName", "FatherName", "Age", "AboutMe", "Status", "Date", "LastActivity","StatusProfile", "Gender", "Wall")
+VALUES (201, 'Secret1', 'Noname', 'Витальевич', 18, 'Холоп светлой стены', 'в активном поиске', to_timestamp('19.05.97', 'dd-mm-yy'), to_timestamp('20:21', 'hh24:mi'), 'онлайн', false, 907),
+(202, null, 'Secret3', null, 50, 'Я сам себе хозяин', 'в активном поиске', to_timestamp('19.05.97', 'dd-mm-yy'), to_timestamp('20:21', 'hh24:mi'), 'онлайн', false, 908);
+
+insert into "Friends" ("IdUser", "IdFriend", "Status", "WideStatus")
+VALUES (101, 102, 0, null),
+(101, 103, 1, null),
+(101, 104, 2, 'Лучшие друзья');
+
+insert into "Dialogs" ("IdDialog", "IdUser", "Status", "Name")
+VALUES (501, 101, 0, null),
+(501, 102, 0, null),
+(502, 103, 0, null),
+(502, 102, 0, null),
+(502, 101, 0, null);
+
+insert into "Group" ("IdGroup", "Name", "Description")
+VALUES (401, 'Танки онлайн', 'Тут играют в танки'),
+(402, 'Just story', 'Тут истории'),
+(403, 'English', 'Тут учат английский');
+
+insert into "UsersGroup" ("IdGroup", "IdUser")
+VALUES (401, 101),
+(401, 102),
+(401, 103),
+(401, 104),
+(402, 102),
+(403, 102),
+(402, 101);
