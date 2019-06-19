873cae8d4928f720334d8aafd61b971931ba4416
Lioran <wersil@mail.ru>
2019-06-19 20:54:34 +0700

    Поставлена защита от злыдня. Добавлен logout и get_profile/<id_user>

--- src/wall.py
+++ src/wall.py
@@ -1,10 +1,10 @@
 from connect import connect
 
 
-def get_user_wall(id_wall):
+def get_user_wall(id_wall, status):
     conn = connect()
     cursor = conn.cursor()
-    cursor.execute('SELECT * FROM "Walls" WHERE "IdWall" = '+id_wall.__str__()+' and "Status" = true')
+    cursor.execute('SELECT * FROM "Walls" WHERE "IdWall" = '+id_wall.__str__()+' and "Status" = '+status.__str__())
     rows = cursor.fetchall()
     result = []
     for row in rows:
